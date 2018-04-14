69e77d1cdbd3d79ec9aeda3f4f59dbe130a9ed83
Feng_Home <edu_feng@163.com>
2018-04-14 16:48:39 +0800

    [add]动态传递新闻数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,15 @@
 //index.js
 
 Page({
+    data: {
+        newsList: [{
+            id: '1523074607642',
+            title: '(Beta)清明假期长三角铁路客流堪比春运',
+            source: '(Beta)环球资讯',
+            date: '2018-04-06T11:28:25.000Z',
+            firstImage: 'http://inews.gtimg.com/newsapp_bt/0/3199649303/641'
+        }]
+    }
+
+
 })
