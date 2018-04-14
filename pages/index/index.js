6d6f2cd82d3ae142109b7318571d63c1bd376205
Feng_Home <edu_feng@163.com>
2018-04-15 00:34:42 +0800

    [add]类别导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,14 @@
 //index.js
 
-
+const categoryMap = {
+    'gn': '国内',
+    'gj': '国际',
+    'cj': '财经',
+    'yl': '娱乐',
+    'js': '军事',
+    'ty': '体育',
+    'other': '其他',
+}
 
 
 Page({
@@ -12,7 +20,10 @@ Page({
             date: '2018-04-06T11:28:25.000Z',
             firstImage: 'http://inews.gtimg.com/newsapp_bt/0/3199649303/641'
         }],
-        category: 'gn'
+        category: 'gn',
+        categoryList: [
+            '国内', '国际', '财经', '娱乐', '军事', '体育', '其他',
+        ]
     },
 
     getNews(callback) {
@@ -38,17 +49,17 @@ Page({
 
         let newsList = []
         for (let i = 0; i < 9; i += 1) {
-          newsList.push({
-              id: result[i].id,
-              title: result[i].title,
-              date: result[i].date,
-              source: result[i].source,
-              firstImage: result[i].firstImage,
-          })
+            newsList.push({
+                id: result[i].id,
+                title: result[i].title,
+                date: result[i].date,
+                source: result[i].source,
+                firstImage: result[i].firstImage,
+            })
         }
         this.setData({
             newsList: newsList
-          })
+        })
         console.log(result)
     },
 
